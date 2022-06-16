b1e6ce3ac6c90a2e9ae873382e4681c3db2c8b20
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2022-06-16 19:29:14 +0300

    Всё доделал
    ура

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
+import collections
+import datetime
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pandas import read_excel
+from pprint import pprint
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+
+foundation_date = datetime.datetime(year=1920, month=3, day=6)
+foundation_year = foundation_date.year
+last_day = datetime.datetime.today()
+last_year = last_day.year
+years_of_existing = last_year - foundation_year
+
+
+env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+)   
+wine_data = read_excel('wine3.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+
+winesorts = wine_data.to_dict(orient='records')
+
+
+menu = collections.defaultdict(list)
+for wine in winesorts:
+    menu[wine['Категория']].append(wine)
+      
+template = env.get_template('template.html')
+
+
+rendered_page = template.render(
+    wine_age=str(years_of_existing),
+    winecards=menu
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+
+server = HTTPServer(
+                ('0.0.0.0', 8000),
+                SimpleHTTPRequestHandler
+            )
 server.serve_forever()
+
