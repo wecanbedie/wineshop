b1502dc39855baa4e0a951568ca8a20d21ae3ee9
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2022-06-23 19:39:06 +0300

    Улучшил весь код в main.py
    - Высушил код
    - Добавил defmain/ifmain
    - Улучшил названия переменных

--- main.py
+++ main.py
@@ -6,42 +6,46 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pandas import read_excel
 
-
-foundation_date = datetime.datetime(year=1920, month=3, day=6)
-foundation_year = foundation_date.year
-last_day = datetime.datetime.today()
-last_year = last_day.year
-years_of_existing = last_year - foundation_year
-
-
-env = Environment(
-            loader=FileSystemLoader('.'),
-            autoescape=select_autoescape(['html', 'xml'])
-)   
-wine_data = read_excel('wine.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
-
-winesorts = wine_data.to_dict(orient='records')
+def main():
+    foundation_date = 1920
+    current_date = datetime.datetime.today()
+    current_year = current_date.year
+    age_of_the_winery = current_year - foundation_date
 
 
-menu = collections.defaultdict(list)
-for wine in winesorts:
-    menu[wine['Категория']].append(wine)
-      
-template = env.get_template('template.html')
+    env = Environment(
+                loader=FileSystemLoader('.'),
+                autoescape=select_autoescape(['html', 'xml'])
+    )   
+    data_from_the_file = read_excel('wine.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+
+    data_drinks_dictionary = data_from_the_file.to_dict(orient='records')
 
 
-rendered_page = template.render(
-    wine_age=str(years_of_existing),
-    winecards=menu
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    menu = collections.defaultdict(list)
+    for wine in data_drinks_dictionary:
+        menu[wine['Категория']].append(wine)
+        
+    template = env.get_template('template.html')
 
 
-server = HTTPServer(
-                ('0.0.0.0', 8000),
-                SimpleHTTPRequestHandler
-            )
-server.serve_forever()
+    rendered_page = template.render(
+        wine_age=age_of_the_winery,
+        winecards=menu
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+    server = HTTPServer(
+                    ('0.0.0.0', 8000),
+                    SimpleHTTPRequestHandler
+                )
+    server.serve_forever()
+
+
+
+if __name__ == '__main__':
+    main()
 
