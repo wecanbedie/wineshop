0312a4c7bc63e4786d763d9debf1564f70124a47
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2022-08-07 18:28:15 +0300

    Исправления
    Названия переменных

--- main.py
+++ main.py
@@ -14,30 +14,30 @@ def main():
     parser.add_argument('filepath', type=str, help='Укажите ваш путь к excel-файлу.')
     arg = parser.parse_args()
 
-    foundation_date = 1920
+    foundation_wineshop = 1920
     current_date = datetime.datetime.today()
     current_year = current_date.year
-    age_of_the_winery = current_year - foundation_date
+    age_winery = current_year - foundation_wineshop
 
 
     env = Environment(
                 loader=FileSystemLoader('.'),
                 autoescape=select_autoescape(['html', 'xml'])
     )   
-    data_from_the_file = read_excel(arg.filepath, sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+    excel_file = read_excel(arg.filepath, sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
 
-    data_drinks_dictionary = data_from_the_file.to_dict(orient='records')
+    data_drinks = excel_file.to_dict(orient='records')
 
 
     menu = collections.defaultdict(list)
-    for wine in data_drinks_dictionary:
+    for wine in data_drinks:
         menu[wine['Категория']].append(wine)
         
     template = env.get_template('template.html')
 
 
     rendered_page = template.render(
-        wine_age=age_of_the_winery,
+        winery_age=age_winery,
         winecards=menu
     )
 
